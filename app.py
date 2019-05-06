eee0dcd916b7a7c2e040205419a84f3ef3ea300a
rmz38 <rmz38@cornell.edu>
2019-05-05 22:57:52 -0400

    added stuff

--- app.py
+++ app.py
@@ -15,6 +15,8 @@ with app.app_context():
 
 
 @app.route('/')
+def getHello():
+    return "HELLOWORLD"
 @app.route('/api/classes/')
 def getClasses():
     classes = Class.query.all()
@@ -34,18 +36,18 @@ def createClass():
     db.session.commit()
     return json.dumps({'success': True, 'data': _class.serializeall()}), 201
 
-@app.route('/api/class/<int:_id>/')
-def getClass(_id):
-    classlist = Class.query.filter_by(id = _id).first()
+@app.route('/api/class/<string:class_code>/')
+def getClass(class_code):
+    classlist = Class.query.filter_by(code = class_code).first()
     if classlist is None:
          return json.dumps({'success': False, 'error': 'Class not found'}), 404
 
     return json.dumps ({'success': True, 'data': classlist.serializeall()}), 200
    
 
-@app.route('/api/class/<int:class_id>/', methods= ['DELETE'])
-def deleteClass(class_id):
-    classlist = Class.query.filter_by(id = class_id).first()
+@app.route('/api/class/<string:class_code>/', methods= ['DELETE'])
+def deleteClass(class_code):
+    classlist = Class.query.filter_by(code = class_code).first()
     if classlist is not None:
         db.session.delete(classlist)
         db.session.commit()
@@ -64,17 +66,17 @@ def createFaculty():
     db.session.commit()
     return json.dumps({'success': True, 'data': faculty.serializeall()}), 201
 
-@app.route('/api/faculty/<int:faculty_id>/')
-def getFaculty(faculty_id):
-    faculty = Faculty.query.filter_by(id = faculty_id).first()
+@app.route('/api/faculty/<string:faculty_name>/')
+def getFaculty(faculty_name):
+    faculty = Faculty.query.filter_by(name = faculty_name).first()
     if faculty is None:
          return json.dumps({'success': False, 'error': 'User not found'}), 404
 
-    return json.dumps ({'success': True, 'data': faculty.serializeall()}), 200
+    return json.dumps ({'success': True, 'data': faculty.serialize()}), 200
 
-@app.route('/api/class/<int:class_id>/add_faculty/', methods = ['POST'])
-def addFaculty(class_id):
-    class_item = Class.query.filter_by(id = class_id).first()
+@app.route('/api/class/<string:class_code>/add_faculty/', methods = ['POST'])
+def addFaculty(class_code):
+    class_item = Class.query.filter_by(code = class_code).first()
     if class_item is None:
         return json.dumps({'success': False, 'error': 'class not found'}), 404
     post_body = json.loads(request.data)
@@ -82,27 +84,31 @@ def addFaculty(class_id):
     class_item.faculty.append(Faculty.query.filter_by(id = faculty_id).first())
         
     db.session.commit()
-    return json.dumps({'success': True, 'data': class_item.serializeid()}), 200
+    return json.dumps({'success': True, 'data': class_item.serializeall()}), 200
 
-@app.route('/api/class/<int:class_id_found>/research/', methods = ['POST'])
-def createResearch(class_id_found):
+@app.route('/api/class/<string:class_code>/research/', methods = ['POST'])
+def createResearch(class_code):
     post_body = json.loads(request.data)
-    class_found = Class.query.filter_by(id = class_id_found).first()
+    class_found = Class.query.filter_by(code = class_code).first()
     if class_found is None:
         return json.dumps({'success': False, 'error': 'class not found'}), 404
     assignment = Research(
-        class_id = class_id_found,
+        class_id = class_found.id,
         description = post_body.get('description'),
         subject = post_body.get('subject')
     )
     db.session.add(assignment)
     db.session.commit()
     return json.dumps({'success': True, 'data': assignment.serialize()}), 201
-
-@app.route('/api/research/<int:research_id>/', methods = ['GET'])
-def getResearch(research_id):
+@app.route('/api/research/', methods = ['GET'])
+def getAllResearch():
+    research = Research.query.all()
+    res = {'success': True, 'data': [r.serialize() for r in research] }
+    return json.dumps(res), 200
+@app.route('/api/research/<string:research_name>/', methods = ['GET'])
+def getResearch(research_name):
     post_body = json.loads(request.data)
-    research_found = Research.query.filter_by(id = research_id).first()
+    research_found = Research.query.filter_by(subject = research_name).first()
     if research_found is None:
         return json.dumps({'success': False, 'error': 'research not found'}), 404
     return json.dumps({'success': True, 'data': research_found.serialize()}), 201
